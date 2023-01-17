1c7f12d00dcd7c4ad93ecea157c63c1461a11b13
Dusan <dusanvulic11@gmail.com>
2023-01-17 22:54:56 +0100

    search filter created[C

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,65 @@
 import React from "react";
 
+//import router
+import { useRouter } from "next/router";
+
+//importing spinner from chakra
+import { Spinner } from "@chakra-ui/react";
+
+//importing function
+import { getFilteredEvents } from "../../dummy-data-js";
+
 const FilteredEvents = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  console.log(filterData);
+
+  if (!filterData) {
+    return (
+      <div className="spinner">
+        {" "}
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </div>
+    );
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>invalid filter,please adjust your search values </p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  console.log(filteredEvents);
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <p>
+        no events found for that search criteria, please adjust your search
+        values{" "}
+      </p>
+    );
+  }
+
   return (
     <div>
       <h3> Filtered Events</h3>
