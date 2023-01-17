6c835ee48530d63a205cb9666651dd757b8f3064
Dusan <dusanvulic11@gmail.com>
2023-01-17 11:40:54 +0100

    single event routing

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,28 @@
 import React from "react";
 
+// importing use router fron next
+import { useRouter } from "next/router";
+
+//function to get filtered event
+import { getEventById } from "../../dummy-data-js";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  const eventId = router.query.eventId;
+
+  const detailedEvent = getEventById(eventId);
+
+  console.log(detailedEvent);
+
+  const { title, image, date, location, id, description } = detailedEvent;
+
   return (
     <div>
-      <h3> Single Event page</h3>
+      <h3> {title}</h3>
+      <p>{date}</p>
     </div>
   );
 };
