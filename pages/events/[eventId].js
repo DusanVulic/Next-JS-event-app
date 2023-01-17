cd4017b8619083036c7767d3f175c8c2fad19568
Dusan <dusanvulic11@gmail.com>
2023-01-17 12:25:13 +0100

    detailed event added and chakra ui loader added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,6 +3,9 @@ import React from "react";
 // importing use router fron next
 import { useRouter } from "next/router";
 
+//importing spinner from chakra
+import { Spinner } from "@chakra-ui/react";
+
 //function to get filtered event
 import { getEventById } from "../../dummy-data-js";
 import DetailedEvent from "../../components/events/DetailedEvent";
@@ -18,10 +21,21 @@ const EventDetailPage = () => {
 
   console.log(event);
 
-  const { title, image, date, location, id, description } = event;
+  //const { title, image, date, location, id, description } = event;
 
   if (!event) {
-    return <div> no event found</div>;
+    return (
+      <div className="spinner">
+        {" "}
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </div>
+    );
   }
 
   return <DetailedEvent {...event} />;
