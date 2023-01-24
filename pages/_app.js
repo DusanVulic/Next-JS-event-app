e7a3705fe512702f4b024d53dd07d9ec46eca428
Dusan <dusanvulic11@gmail.com>
2023-01-24 12:05:01 +0100

    adding metadata in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,20 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
 
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
